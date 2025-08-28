42f5ba215934ceaedf2079c40394aa00b08c99a0
Tayshaun M. Delos Santos <tayshaunds25@gmail.com>
2025-08-28 23:24:53 +0800

    Setting up resurrect and workspace switcher plugin

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,7 +2,85 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- Load Plugins
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+
 config.default_prog = { "/bin/zsh" }
+
+-- === Plugin Configuration ===
+
+-- loads the state whenever I create a new workspace
+wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, path, label)
+    local workspace_state = resurrect.workspace_state
+
+    workspace_state.restore_workspace(resurrect.state_manager.load_state(label, "workspace"), {
+        window = window,
+        relative = true,
+        restore_text = true,
+        on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+    })
+end)
+
+-- Saves the state whenever I select a workspace
+wezterm.on("smart_workspace_switcher.workspace_switcher.selected", function(window, path, label)
+    local workspace_state = resurrect.workspace_state
+    resurrect.state_manager.save_state(workspace_state.get_workspace_state())
+end)
+
+-- Add entries in command palette for renaming and switching workspaces
+wezterm.on("augment-command-palette", function(window, pane)
+    local workspace_state = resurrect.workspace_state
+    return {
+        {
+            brief = "Window | Workspace: Switch Workspace",
+            icon = "md_briefcase_arrow_up_down",
+            action = workspace_switcher.switch_workspace(),
+        },
+        {
+            brief = "Window | Workspace: Rename Workspace",
+            icon = "md_briefcase_edit",
+            action = wezterm.action.PromptInputLine({
+                description = "Enter new name for workspace",
+                action = wezterm.action_callback(function(window, pane, line)
+                    if line then
+                        wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+                        resurrect.state_manager.save_state(workspace_state.get_workspace_state())
+                    end
+                end),
+            }),
+        },
+    }
+end)
+
+-- Add the selected path to the right status bar when choosing a workspace
+wezterm.on("smart_workspace_switcher.workspace_switcher.chosen", function(window, workspace)
+    local gui_win = window:gui_window()
+    local base_path = string.gsub(workspace, "(.*[/\\])(.*)", "%2")
+    gui_win:set_right_status(wezterm.format({
+        { Foreground = { Color = "green" } },
+        { Text = base_path .. "  " },
+    }))
+end)
+
+wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, workspace)
+    local gui_win = window:gui_window()
+    local base_path = string.gsub(workspace, "(.*[/\\])(.*)", "%2")
+    gui_win:set_right_status(wezterm.format({
+        { Foreground = { Color = "green" } },
+        { Text = base_path .. "  " },
+    }))
+end)
+
+-- Workspace Formatter
+workspace_switcher.workspace_formatter = function(label)
+    return wezterm.format({
+        { Attribute = { Italic = true } },
+        { Foreground = { Color = "#272e33" } },
+        { Text = "󱂬: " .. label },
+    })
+end
+
 
 -- === Appearance & Theme ===
 config.window_background_opacity = 0.9
@@ -88,15 +166,15 @@ config.use_fancy_tab_bar = false
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
     if tab.is_active then
         return {
-        {Foreground={Color=C_HL_1}},
-        {Text=" " .. tab.tab_index+1},
-        {Foreground={Color=C_HL_2}},
-        {Text=": "},
-        {Foreground={Color=C_ACTIVE_FG}},
-        {Text=tab.active_pane.title .. " "},
-        {Background={Color=C_BG}},
-        {Foreground={Color=C_HL_1}},
-        {Text="|"},
+            {Foreground={Color=C_HL_1}},
+            {Text=" " .. tab.tab_index+1},
+            {Foreground={Color=C_HL_2}},
+            {Text=": "},
+            {Foreground={Color=C_ACTIVE_FG}},
+            {Text=tab.active_pane.title .. " "},
+            {Background={Color=C_BG}},
+            {Foreground={Color=C_HL_1}},
+            {Text="|"},
         }
     end
     return {
@@ -164,7 +242,7 @@ config.keys = {
     -- } },
 
     -- Navigate tabs
-    { key = 'w', mods = 'LEADER', action = act.ShowTabNavigator },
+    { key = 't', mods = 'LEADER', action = act.ShowTabNavigator },
     -- Next/Previous tab
     { key = 'Tab', mods = 'CTRL', action = act.ActivateTabRelative(1) },
     { key = 'Tab', mods = 'SHIFT|CTRL', action = act.ActivateTabRelative(-1) },
@@ -216,6 +294,94 @@ config.keys = {
     -- Scroll to top/bottom
     { key = 'Home', mods = 'SHIFT', action = act.ScrollToTop },
     { key = 'End', mods = 'SHIFT', action = act.ScrollToBottom },
+
+    -- === Workspace Management ===
+
+    -- Switch workspace
+    {
+        key = "s",
+        mods = "LEADER",
+        action = workspace_switcher.switch_workspace(),
+    },
+    {
+        key = "S",
+        mods = "LEADER",
+        action = workspace_switcher.switch_to_prev_workspace(),
+    },
+
+    -- Saving workspace
+    {
+        key = "w",
+        mods = "LEADER",
+        action = wezterm.action_callback(function(win, pane)
+            resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+        end),
+    },
+    {
+        key = "W",
+        mods = "LEADER",
+        action = resurrect.window_state.save_window_action(),
+    },
+    {
+        key = "T",
+        mods = "LEADER",
+        action = resurrect.tab_state.save_tab_action(),
+    },
+    {
+        key = "s",
+        mods = "CTRL",
+        action = wezterm.action_callback(function(win, pane)
+            resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+            resurrect.window_state.save_window_action()
+        end),
+    },
+
+    -- Loading workspace
+    {
+        key = "r",
+        mods = "LEADER",
+        action = wezterm.action_callback(function(win, pane)
+            resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
+                local type = string.match(id, "^([^/]+)") -- match before '/'
+                id = string.match(id, "([^/]+)$") -- match after '/'
+                id = string.match(id, "(.+)%..+$") -- remove file extention
+                local opts = {
+                    relative = true,
+                    restore_text = true,
+                    on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+                    close_open_tabs = true,
+                    window = pane:window(),
+                }
+                if type == "workspace" then
+                    local state = resurrect.state_manager.load_state(id, "workspace")
+                    resurrect.workspace_state.restore_workspace(state, opts)
+                elseif type == "window" then
+                    local state = resurrect.state_manager.load_state(id, "window")
+                    resurrect.window_state.restore_window(pane:window(), state, opts)
+                elseif type == "tab" then
+                    local state = resurrect.state_manager.load_state(id, "tab")
+                    resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+                end
+            end)
+        end),
+    },
+
+    -- Deleting workspace
+    {
+        key = "d",
+        mods = "LEADER",
+        action = wezterm.action_callback(function(win, pane)
+            resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id)
+                resurrect.state_manager.delete_state(id)
+            end,
+            {
+                title = "Delete State",
+                description = "Select State to Delete and press Enter = accept, Esc = cancel, / = filter",
+                fuzzy_description = "Search State to Delete: ",
+                is_fuzzy = true,
+            })
+        end),
+    },
 }
 
 return config
