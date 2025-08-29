2b6463e4bb754af69efa90f867bf383050a2dedd
Tayshaun M. Delos Santos <tayshaunds25@gmail.com>
2025-08-29 14:13:15 +0800

    Simpler workspace implementation

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -262,143 +262,27 @@ config.keys = {
     -- === Workspace Management ===
 
     -- Create new workspace
-    { key = 'w', mods = 'CTRL|SHIFT', action = act.PromptInputLine {
-        description = wezterm.format {
-            { Attribute = { Intensity = 'Bold' } },
-            { Foreground = { Color = C_HL_1 } },
-            { Text = 'Enter name for new workspace:' },
-        },
-        action = wezterm.action_callback(function(window, pane, line)
-            if line then
-                window:perform_action(
-                    act.SwitchToWorkspace {
-                        name = line,
-                    },
-                    pane
-                )
-            end
-        end),
-    }},
+    { key = 's', mods = 'LEADER', action = act.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' } },
 
-
-    -- Delete workspace
-    { key = 'd', mods = 'CTRL|SHIFT', action = act.PromptInputLine {
-        description = wezterm.format {
-            { Attribute = { Intensity = 'Bold' } },
-            { Foreground = { Color = C_HL_2 } },
-            { Text = 'Enter name of workspace to delete:' },
-        },
-        action = wezterm.action_callback(function(window, pane, line)
-            if line then
-                window:perform_action(
-                    act.RemoveWorkspace(line),
-                    pane
-                )
-            end
-        end),
-    }},
+    -- Saving a workspace
+    { key = 's', mods = 'CTRL', action = act.SwitchToWorkspace { name = 'default' } },
 
     -- Rename workspace
     { key = 'r', mods = 'LEADER', action = wezterm.action.PromptInputLine({
-        description = "Enter new name for workspace",
+        description = wezterm.format {
+            { Attribute = { Intensity = 'Bold' } },
+            { Foreground = { Color = C_HL_1 } },
+            { Text = 'Enter new name for the workspace:' },
+        },
         action = wezterm.action_callback(function(window, pane, line)
             if line then
                 wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
-                resurrect.state_manager.save_state(workspace_state.get_workspace_state())
             end
         end),
         }),
     },
 
-    -- WORKING WORKSPACE SWITCHER
-    { key = '9', mods = 'ALT', action = act.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' } },
-
-
-    -- Switch workspace
-    {
-        key = "s",
-        mods = "LEADER",
-        action = workspace_switcher.switch_workspace(),
-    },
-    {
-        key = "S",
-        mods = "LEADER",
-        action = workspace_switcher.switch_to_prev_workspace(),
-    },
-
-    -- Saving workspace
-    {
-        key = "s",
-        mods = "CTRL",
-        action = wezterm.action_callback(function(win, pane)
-            resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
-            resurrect.window_state.save_window_action()
-        end),
-    },
-    {
-        key = "w",
-        mods = "LEADER",
-        action = wezterm.action_callback(function(win, pane)
-            resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
-        end),
-    },
-    {
-        key = "W",
-        mods = "LEADER",
-        action = resurrect.window_state.save_window_action(),
-    },
-    {
-        key = "T",
-        mods = "LEADER",
-        action = resurrect.tab_state.save_tab_action(),
-    },
-
-    -- Loading workspace
-    {
-        key = "l",
-        mods = "LEADER",
-        action = wezterm.action_callback(function(win, pane)
-            resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
-                local type = string.match(id, "^([^/]+)") -- match before '/'
-                id = string.match(id, "([^/]+)$") -- match after '/'
-                id = string.match(id, "(.+)%..+$") -- remove file extention
-                local opts = {
-                    relative = true,
-                    restore_text = true,
-                    on_pane_restore = resurrect.tab_state.default_on_pane_restore,
-                    close_open_tabs = true,
-                    window = pane:window(),
-                }
-                if type == "workspace" then
-                    local state = resurrect.state_manager.load_state(id, "workspace")
-                    resurrect.workspace_state.restore_workspace(state, opts)
-                elseif type == "window" then
-                    local state = resurrect.state_manager.load_state(id, "window")
-                    resurrect.window_state.restore_window(pane:window(), state, opts)
-                elseif type == "tab" then
-                    local state = resurrect.state_manager.load_state(id, "tab")
-                    resurrect.tab_state.restore_tab(pane:tab(), state, opts)
-                end
-            end)
-        end),
-    },
-
-    -- Deleting workspace
-    {
-        key = "d",
-        mods = "LEADER",
-        action = wezterm.action_callback(function(win, pane)
-            resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id)
-                resurrect.state_manager.delete_state(id)
-            end,
-            {
-                title = "Delete State",
-                description = "Select State to Delete and press Enter = accept, Esc = cancel, / = filter",
-                fuzzy_description = "Search State to Delete: ",
-                is_fuzzy = true,
-            })
-        end),
-    },
+    -- To delete a workspace, do not hit ctrl+s when leaving a workspace
 }
 
 return config
